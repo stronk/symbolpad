9e08e40ea01ba812d3ca4b08e7417caa71b8ba40
Koen Smeets <koen@nieuwsmedium.nl>
2026-03-17 10:57:08 +0100

    Fix disappearing app?

--- src-tauri/src/main.rs
+++ src-tauri/src/main.rs
@@ -1,11 +1,15 @@
 #![cfg_attr(not(debug_assertions), windows_subsystem = "windows")]
 
+
+
 use tauri::{
     image::Image,
     tray::{TrayIconBuilder, TrayIconEvent},
     Manager, WindowEvent,
 };
 use tauri_plugin_positioner::{Position, WindowExt};
+use std::sync::{Arc, Mutex};
+use std::time::{Duration, Instant};
 
 fn main() {
     tauri::Builder::default()
@@ -43,7 +47,39 @@ fn main() {
                 .build(app)?;
 
             Ok(())
-        })
+        
+		let window = app.get_webview_window("main").unwrap();
+		let win_blur = window.clone();
+		let last_shown = Arc::new(Mutex::new(Instant::now() - Duration::from_secs(10)));
+		let last_shown_blur = last_shown.clone();
+		
+		window.on_window_event(move |event| {
+			if let WindowEvent::Focused(false) = event {
+				let elapsed = last_shown_blur.lock().unwrap().elapsed();
+				if elapsed > Duration::from_millis(200) {
+					let _ = win_blur.hide();
+				}
+			}
+		});
+		
+		let win_tray = window.clone();
+		TrayIconBuilder::new()
+			.icon(icon)
+			.icon_as_template(true)
+			.on_tray_icon_event(move |_tray, event| {
+				if let TrayIconEvent::Click { .. } = event {
+					if win_tray.is_visible().unwrap_or(false) {
+						let _ = win_tray.hide();
+					} else {
+						*last_shown.lock().unwrap() = Instant::now();
+						let _ = win_tray.move_window(Position::TrayCenter);
+						let _ = win_tray.show();
+						let _ = win_tray.set_focus();
+					}
+				}
+			})
+			.build(app)?;
+		})
         .run(tauri::generate_context!())
         .expect("error running SymbolPad");
 }
